dd7d0085cc2eeffc171684fe22a99338dcf14277
Annab <annabplr@gmail.com>
2019-08-05 14:47:11 +0200

    wip - replace snippet label by snippet.label.id

--- db/migrate/20190722070023_remove_label_id_from_snippet.rb
+++ db/migrate/20190722070023_remove_label_id_from_snippet.rb
@@ -1,5 +1,10 @@
 class RemoveLabelIdFromSnippet < ActiveRecord::Migration[5.2]
   def change
+    Snippet.where.not(label_id: [nil, ""]).each do |snippet|
+      label = Label.find_or_create_by(name: snippet.label.strip.upcase)
+      labeling = label.labelings.build(snippet: snippet)
+      labeling.save
+    end
     remove_column :snippets, :label_id
   end
 end
