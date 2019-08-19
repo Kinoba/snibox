abe3918dd8e1d576956f6e1747444ee918afab8c
Annab <annabplr@gmail.com>
2019-08-19 14:03:49 +0200

    update snippet count to 0 when migrating datas

--- db/migrate/20190722070023_remove_label_id_from_snippet.rb
+++ db/migrate/20190722070023_remove_label_id_from_snippet.rb
@@ -1,5 +1,6 @@
 class RemoveLabelIdFromSnippet < ActiveRecord::Migration[5.2]
   def change
+    Label.update_all(snippets_count: 0)
     Snippet.where.not(label_id: nil).each do |snippet|
       label = Label.find_by(id: snippet.label_id)
       labeling = label.labelings.build(snippet: snippet)
