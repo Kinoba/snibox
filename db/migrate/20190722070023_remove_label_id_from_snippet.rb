0c39bb92374bb45523c376604f9787618b18adca
Annab <annabplr@gmail.com>
2019-08-19 12:16:30 +0200

    migration label_id

--- db/migrate/20190722070023_remove_label_id_from_snippet.rb
+++ db/migrate/20190722070023_remove_label_id_from_snippet.rb
@@ -1,9 +1,9 @@
 class RemoveLabelIdFromSnippet < ActiveRecord::Migration[5.2]
   def change
-    Snippet.where.not(label_id: [nil, ""]).each do |snippet|
-      label = Label.find_or_create_by(name: snippet.label.strip.upcase)
+    Snippet.where.not(label_id: nil).each do |snippet|
+      label = Label.find_by(id: snippet.label_id)
       labeling = label.labelings.build(snippet: snippet)
-      labeling.save
+      labeling.save!
     end
     remove_column :snippets, :label_id
   end
